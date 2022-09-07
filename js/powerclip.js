e9c7c052600f60b815101ce3d56800592a7e691f
David Anderson <davidans2019@gmail.com>
2022-09-07 16:11:37 +0100

    122

--- js/powerclip.js
+++ js/powerclip.js
@@ -2,12 +2,15 @@
 
 //set function for the mode buttons
 function setMode(mode){
-    for(let i=0; i<2; i++){
+    for(let i=0; i<1; i++){
         document.querySelector('.slide_content').style.display = 'none'
         document.querySelector('.lw3_content').style.display = 'none'
+        document.querySelector('.mode_slide').classList.remove('modeActive')
+        document.querySelector('.mode_lw3').classList.remove('modeActive')
     }
     document.querySelector(`.${mode}_content`).style.display = 'block'
-    document.querySelector(`.mode_${mode}`).classList.add('mode_button_active')
-    console.log(mode)
+    document.querySelector(`.mode_${mode}`).classList.add('modeActive')
+
 }
+
 console.log(33)
