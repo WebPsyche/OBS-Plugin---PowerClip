181947fa6669b1170a44dbd9f8f0d4d022cab425
David Anderson <davidans2019@gmail.com>
2022-09-08 23:11:39 +0100

    aer

--- js/powerclip.js
+++ js/powerclip.js
@@ -1,16 +1,2 @@
-//power clip 1.0
-
-//set function for the mode buttons
-function setMode(mode){
-    for(let i=0; i<1; i++){
-        document.querySelector('.slide_content').style.display = 'none'
-        document.querySelector('.lw3_content').style.display = 'none'
-        document.querySelector('.mode_slide').classList.remove('modeActive')
-        document.querySelector('.mode_lw3').classList.remove('modeActive')
-    }
-    document.querySelector(`.${mode}_content`).style.display = 'block'
-    document.querySelector(`.mode_${mode}`).classList.add('modeActive')
-
-}
 
 console.log(33)
